334e355e5c33e84c6803fd797775d6e3dc81fa75
Marc Hernandez <drstein21@live.com>
2022-11-10 16:33:52 -0600

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //boutta rip this shit cuz
 
 const express = require('express');
+const { client } = require('./db/index')
 // setting up this express shit
 const app = express();
+
+// connect this too, or else
+client.connect();
 
 // Set up the damn port so it listens to the server
 app.listen(3000, () => {
