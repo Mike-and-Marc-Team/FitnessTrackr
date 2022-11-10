fdca9ff81028bc13f5d891397208a192bfb2ad9f
Marc Hernandez <drstein21@live.com>
2022-11-10 16:32:44 -0600

    setup express and pg client infrast-whatever

--- db/index.js
+++ db/index.js
@@ -1,1 +1,12 @@
 //pg.client go here
+
+//1: Import pg
+const pg = require('pg');
+
+//2: Make a new pg.client instance
+const client = new pg.Client(`postgres://localhost:5432/fitness-dev`);
+
+//3: Export the db client
+module.exports = {
+    client
+}
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //boutta rip this shit cuz
+
+const express = require('express');
+// setting up this express shit
+const app = express();
+
+// Set up the damn port so it listens to the server
+app.listen(3000, () => {
+    console.log('Ripping it up in port 3000');
+})
