38bffd839ddd64c033dd0187e405e6fe9722f8b9
ItsMashBro <M.Ashley365@Gmail.Com>
2022-11-16 15:45:20 -0600

    routines.js needs /:routineId/activities

--- api/routines.js
+++ api/routines.js
@@ -5,7 +5,7 @@ const {requireUser } = require('./utilities');
 
 const {
     createRoutine,
-    getAllRoutine,
+    getAllRoutines,
     updateRoutine,
     getRoutineById
 } = require('../db');
@@ -19,13 +19,113 @@ routineRouter.use((req, res, next) =>{
 
 
 routineRouter.get("/", async (req, res) => {
-    const routine = await getAllRoutine();
-    console.log("Who are you people");
-    res.send({
-        users
-    })
+    try {
+        const allRoutines = await getAllRoutines();
+
+        const routines = allRoutines.filter(routine => {
+            if (routine.active) {
+                return true;
+            }
+            if (req.user && routine.author.id === req.user.id) {
+                return true;
+            }
+            return false;
+        });
+        console.log("Who are you people");
+
+        res.send({
+            routines
+        });
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
 });
 
 routineRouter.post("/", requireUser, async (req, res, next) =>{
-    
-})
+    const { title, content, activities = "" } = req.body;
+
+    const activityArr = activities.trim().split(/\s+/)
+    const postData = {};
+
+    if (activityArr.length) {
+        postData.activities = activityArr;
+    }
+
+    try {
+        postData.authorId = req.user.id;
+        postData.title = title;
+        postData.content = content;
+
+        const post = await createRoutine(postData);
+
+        if (post) {
+            res.send(post);
+        } else {
+            next({
+                name: "PostCreationError",
+                message: "There was an error creating your post. Please try again."
+            })
+        }
+    } catch ({name, message}) {
+        next({name, message});
+    }
+});
+
+routineRouter.patch('/:routineId', requireUser, async (req, res, next) => {
+    const { routineId } = req.params;
+    const { title, content, activities } = req.body;
+
+    const updateFields = {}
+
+    if (activities && activities.length > 0) {
+        updateFields.activities = activities.trim().split(/\s+/);
+    }
+    if (title) {
+        updateFields.title = title;
+    }
+    if (content) {
+        updateFields.content = content;
+    }
+
+    try {
+        const originalRoutine = await getRoutineById(routineId);
+
+        if (originalRoutine.author.id === req.user.id) {
+            const updatedRoutine = await updateRoutine(routineId, updateFields);
+            res.send({ post: updatedRoutine })
+        } else {
+            next({
+                name: "UnauthorizedUserError",
+                message: "You cannot update a post that is not yours"
+            })
+        }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
+routineRouter.delete('/:routineId', requireUser, async (req, res, next) => {
+    try {
+        const routine = await getRoutineById(req.params.routineId);
+
+        if (routine && routine.author.id === req.user.id) {
+            const updatedRoutine = await updateRoutine(routine.id, { active: false });
+
+            res.send({ post: updatedRoutine});
+        } else {
+            next(post ? {
+                name: "UnauthorizedUserError",
+                message: "You cannot delete a post which is not yours"
+            } : {
+                name: "PostNotFoundError",
+                message: "That post does not exist"
+            });
+        }
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+});
+
+
+
+module.exports = routineRouter;
